32d2fc07cce179fbf4ff10cd1d561ad6ab860f04
1043806046 <1043806046@qq.com>
2015-04-08 23:29:42 +0800

    小做改动

--- src/ui/JFrameTotal.java
+++ src/ui/JFrameTotal.java
@@ -12,29 +12,32 @@ import javax.swing.JFrame;
 
 public abstract class JFrameTotal extends JFrame{
 	//TODO 标题栏名称
-	protected static final String title = "ELPrject";
-	//界面宽度
+	protected static final String TITLE = "ELPrject";
+	/*//界面宽度
 	private static final int frameWidth = 1162;
 	//界面高度
-	private static final int frameHeight = 654;
+	private static final int frameHeight = 654;*/
 	//减去屏幕任务栏误差
-	private static final int windowUp = 16;
+	private static final int WINDOW_UP = 16;
 	
 	public JFrameTotal(){
 		//设置标题栏名称
-		this.setTitle(title);
-		//设置界面大小
-		this.setSize(frameWidth, frameHeight);
+		this.setTitle(TITLE);
+		
 		//设置不可改变大小以及关闭后停止运行
 		this.setResizable(false);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		//将界面置于显示屏正中央
+		
 		Toolkit toolkit = Toolkit.getDefaultToolkit();
 		Dimension screen = toolkit.getScreenSize();
+		//设置界面大小
+		this.setSize((int)(screen.width*0.618), (int)(screen.height*0.618));     //0.618是黄金分割比
+		//将界面置于显示屏正中央
 		int x = screen.width-this.getWidth()>>1;
-		int y = (screen.height-this.getHeight()>>1)-windowUp;
+		int y = (screen.height-this.getHeight()>>1)-WINDOW_UP;
 		this.setLocation(x, y);
+		
 		
 		//设置可以显示
 		this.setVisible(true);
